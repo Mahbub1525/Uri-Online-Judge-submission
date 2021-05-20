c80218ef6f43394bcd5d09e7d2f69eef8fb04405
Kazi Golam Mahbub <ovim1525@gmail.com>
2021-05-21 05:12:27 +0600

    Accepted

---
+++ 1008/Salary.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int A;
+	float B,C,D;
+	
+	scanf("%d",&A);
+	scanf("%f",&B);
+	scanf("%f",&C);
+	
+	D=(B*C);
+	
+	printf("NUMBER = %d\n",A);
+	printf("SALARY = U$ %.2f\n",D);
+	
+	return 0;
+}
