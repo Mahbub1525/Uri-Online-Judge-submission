c2f3dc12c76dc185ab7fff0c232852207c7c144b
Kazi Golam Mahbub <ovim1525@gmail.com>
2021-05-21 05:11:41 +0600

    Accepted

---
+++ 1007/Difference.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int A,B,C,D,X;
+	
+	scanf("%d",&A);
+	scanf("%d",&B);
+	scanf("%d",&C);
+	scanf("%d",&D);
+	X=(A*B)-(C*D);
+	
+	printf("DIFERENCA = %d\n",X);
+	
+	return 0;
+}
